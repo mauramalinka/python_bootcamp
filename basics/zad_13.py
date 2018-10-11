e1ddcf7236425d93f60995bbc9fd0d877aa8b471
mauramalinska <mmalinska@chem.uw.edu.pl>
2018-10-11 13:31:19 +0200

    poprawka zad13

--- basics/zad_13.py
+++ basics/zad_13.py
@@ -1,10 +1,14 @@
 dane = 0
 i = 0
+x= 0
 while True:
-    dane = int(input('Podaj temperature T lub wpisz k to zakonczy:'))
-    if T == "k":
+    dane = input('Podaj temperature T lub wpisz k to zakonczy:')
+    if dane != "k":
+        dane = float(dane)
+        x += dane
+        i += 1
+        srednia = x / i
+    else:
         break
-    srednia += float(dane)
-    i += 1
-    print(f"Srednia T:= {srednia/i+1}")
+print(f"Srednia T = {srednia}")
 

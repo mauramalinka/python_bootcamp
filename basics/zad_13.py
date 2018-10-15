0e7cda08b68c551a30420a67dadfda49a34d1d5c
mauramalinska <mmalinska@chem.uw.edu.pl>
2018-10-15 17:04:20 +0200

    poprawki do zad13

--- basics/zad_13.py
+++ basics/zad_13.py
@@ -2,7 +2,7 @@ dane = 0
 i = 0
 x= 0
 while True:
-    dane = input('Podaj temperature T lub wpisz k to zakonczy:')
+    dane = input('Podaj temperature T lub wpisz k by zakonczy:')
     if dane != "k":
         dane = float(dane)
         x += dane

c2ff3462a653aa23b50c6d760eff2b2bf0e5cb71
maura <mauramalinka@gmail.com>
2018-10-17 20:32:57 +0200

    17.10.2018

--- czesc2/zad4.py
+++ czesc2/zad4.py
@@ -1,8 +1,16 @@
 ilosc = 0
+
 for x in range(101):
     if x % 3 == 0 or x % 5 == 0:
         print(x)
         ilosc += 1
 
-print(f"ilosc liczb podzielnych przez 3 i 5 w zakresie od 0 do 100 jest równa: {ilosc}")
+print(f"ilosc liczb podzielnych przez 3 lub 5 w zakresie od 0 do 100 jest równa: {ilosc}")
 
+lista = []
+for x in range(101):
+    if x % 3 == 0 or x % 5 == 0:
+        lista.append(x)
+
+print(len(lista))
+print(lista)
---
+++ czesc2/zad5.py
@@ -0,0 +1,15 @@
+
+#lista = list(range(10))
+#range = range(10)
+
+for i in range(10):
+    for j in range(10):
+        x = i
+        y = j
+        w = i * j
+        print(f"{w:3}", end = " ")
+        #print(x)
+        #print(y)
+
+
+#print(f"{x:3}", end = " ")

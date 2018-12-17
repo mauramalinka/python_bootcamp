c1443338637204b3e7b6586104eca8d97818e0e2
maura <mauramalinka@gmail.com>
2018-12-17 20:29:29 +0100

    17.12.2018

--- web_apps/examples/maths/models.py
+++ web_apps/examples/maths/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Math (models.Model):
+    calculate = models.CharField(max_length=4) #pierwsza kolumna
+    a = models.IntegerField()
+    b = models.IntegerField()
+    c = models.TextField()
+    created = models.DateTimeField(auto_now=True)
--- web_apps/examples/maths/views.py
+++ web_apps/examples/maths/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from maths.models import Math
+
 
 def calculate(request, dzialanie, a , b):
+    a, b = int(a), int(b)
+
     if dzialanie == 'add':
-        c = int(a) + int(b)
+        c = a + b
     elif dzialanie == 'sub':
-        c = int(a) - int(b)
+        c = a - b
     elif dzialanie == 'div':
         if b == 0:
             c = "Nie dziel przez zero cholero"
         else:
-            c = int(a) / int(b)
+            c = a / b
     elif dzialanie == 'mul':
-        c = int(a) * int(b)
+        c = a * b
+    m = Math(calculate='dzialanie', a=a, b=b, c=c)
+    m.save()
     return HttpResponse(f"Wynik = {c}")

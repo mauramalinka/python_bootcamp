120bb90168d295dfc5e5088c355fac1c67c230aa
maura <mauramalinka@gmail.com>
2019-01-07 20:33:04 +0100

    07.01.2019

--- web_apps/biblioteka/books/models.py
+++ web_apps/biblioteka/books/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
+class Author(models.Model):
+    imie = models.CharField(max_length=200)
+    nazwisko = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.imie}-{self.nazwisko}"
 
 class Book(models.Model):
     title = models.CharField(max_length=200) #pierwsza kolumna
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
     cover = models.ImageField(upload_to="books_covers/%Y/%m/%d")
 
     def __str__(self):
         return f"{self.title}-{self.author}"
+
 
 # Create your models here.
